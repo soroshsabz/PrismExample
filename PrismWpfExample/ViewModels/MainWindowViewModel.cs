325541eaf90403e9471ba14adff61b2dc2dd6ccc
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2019-08-23 16:35:22 +0430

    Add some unicode example

--- PrismWpfExample/ViewModels/MainWindowViewModel.cs
+++ PrismWpfExample/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,37 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace PrismWpfExample.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+		public MainWindowViewModel()
+        {
+			ShowDateStringCommand = new DelegateCommand(ShowDateString);
+			DateString = "خالی";
+        }
+
+		void ShowDateString()
+		{
+			DateString = "سلام علیکم";
+		}
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
-        {
 
-        }
+		public string DateString
+		{
+			get => _dateString;
+			set => SetProperty(ref _dateString, value);
+		}
+
+		public DelegateCommand ShowDateStringCommand { get; private set; }
+
+		private string _dateString;
+        private string _title = "Prism Application";
     }
 }
