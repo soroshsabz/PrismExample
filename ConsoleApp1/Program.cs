e8e11bf934947eb6beb58566378e699273689eb4
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2019-08-23 14:44:56 +0430

    Add base projects

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using MD.PersianDateTime;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Threading.Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("fa-IR");
+			PersianCalendar persianCalender = new PersianCalendar();
+			Console.WriteLine(DateTime.Now.ToString(CultureInfo.CreateSpecificCulture("fa-IR")));
+			Console.WriteLine(PersianDateTime.Now.ToString());
+			Console.WriteLine("سلام علیکم");
+			Console.ReadKey();
+		}
+	}
+}
